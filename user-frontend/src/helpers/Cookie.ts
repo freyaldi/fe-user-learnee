5bcf69048f35392837a068e373d17c26ba9150c7
Ferza Reyaldi <ferza.reyaldi@shopee.com>
2023-05-04 11:27:53 +0700

    feat: added setCookie and getCookie

---
+++ user-frontend/src/helpers/Cookie.ts
@@ -0,0 +1,19 @@
+export function setCookie(cname: string, cvalue: string, expireAt: Date): void {
+  const expires = "expires=" + expireAt.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function getCookie(cname: string): string {
+  const name = cname + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
